fe83f8ba6dccaa6c7382903b684b363cc0c75580
borepstein <borepstein@gmail.com>
2021-12-11 21:00:24 -0500

    nn_model removed. Major refactoring by ZuluPro integrated.

--- py_2048_rl/flex_rl_model/agent.py
+++ py_2048_rl/flex_rl_model/agent.py
@@ -1,213 +1,209 @@
-import tensorflow as tf2
+import logging
+
+import tensorflow as tf
 import numpy as np
-import os
+
 import py_2048_rl.game.game as game
-import py_2048_rl.logging.logger as logger
-import py_2048_rl.flex_rl_model.episodes as episodes
-import py_2048_rl.flex_rl_model.callbacks as callbacks
+from py_2048_rl.flex_rl_model import episodes
 
-#--- BEGIN FlexSequentialModel ---
-class FlexSequentialModel(tf2.keras.models.Sequential):
-  pass
-#--- END FlexSequentialModel ---
-
-class Agent():
-  def __init__(self, **kwargs):
-    self.__hash = {}
-    self.__hash["batch_size"] = 10000
-    self.__hash["mem_size"] = 50000
-    self.__hash["input_dims"] = [16]
-    self.__hash["lr"] = 0.001
-    self.__hash["gamma"] = 0.99
-    self.__hash["gamma1"] = 0.99
-    self.__hash["n_actions"] = 4
-    self.__hash["epsilon"] = 1
-    self.__hash["epsilon_dec"] = 1e-3
-    self.__hash["epsilon_min"] = 0.01
-    self.__hash["model_load_file"] = None
-    self.__hash["model_save_file"] = 'model.h5'
-    self.__hash["model_auto_save"] = True
-    self.__hash["log_dir"] = "/app/logs"
-    self.__hash["tf_proc_debug"] = False
-    self.__hash["training_epochs"] = 1
-
-    for k in kwargs.keys():
-      self.__hash[k] = kwargs[k]
-
-    """
-    Special parameters.
-    """
-    if "model" not in self.__hash.keys():
-      self.__create_default_model()
-
-    if "episode_db" not in self.__hash.keys():
-      self.__hash["episode_db"] = episodes.EdpisodeDB(self.__hash["mem_size"],\
-                                                      self.__hash["input_dims"],
-                                                      tf_proc_debug=self.__hash["tf_proc_debug"])
-
-    self.__hash["last_game_score"] = 0
-    self.__hash['last_game_num_moves'] = 0
-    self.__hash["logger"] = logger.Logger()
-
-    self.__hash["tensorboard_callback"] =\
-      tf2.keras.callbacks.TensorBoard(log_dir=self.__hash["log_dir"],
-                                      histogram_freq=1,
-                                      profile_batch = '500,520')
-
-    tf2.profiler.experimental.server.start( 6009 )
-
-    self.__hash["training_histories"] = []
-
-    tf2.debugging.set_log_device_placement(self.__hash["tf_proc_debug"])
-    if self.__hash["tf_proc_debug"]:
-      tf2.debugging.experimental.enable_dump_debug_info(self.__hash["log_dir"],
-                                                        tensor_debug_mode="FULL_HEALTH",
-                                                        circular_buffer_size=-1)
-
-  def __create_default_model(self):
-    model: tf2.keras.models.Sequential = tf2.keras.Sequential([
-      tf2.keras.layers.Dense(16, activation='relu'),
-      tf2.keras.layers.Dense(64, activation='relu'),
-      tf2.keras.layers.Dense(self.__hash["n_actions"], activation=None)])
-
-    model.compile(optimizer=tf2.keras.optimizers.Adam(learning_rate=self.__hash["lr"]),
-                  loss='mean_squared_error')
-
-    self.__hash["model"] = model
-
-  def learn(self, run):
-    self.accumulate_episode_data()
-    ep_db = self.__hash["episode_db"]
-    m1 = self.__hash["model"]
-#    tbcb = self.__hash["tensorboard_callback"]
-    tf2.debugging.set_log_device_placement(self.__hash["tf_proc_debug"])
-
-    states, states_, actions, rewards, scores, dones = \
-      ep_db.get_random_data_batch(self.__hash['batch_size'])
-
-    q_eval = tf2.Variable(tf2.constant(m1.predict(states.numpy())))
-    q_next = tf2.Variable(tf2.constant(m1.predict(states_.numpy())))
-    q_target = q_eval.numpy()
-    batch_index = np.arange(self.__hash['batch_size'])
-    q_target[batch_index, actions] = rewards + self.__hash["gamma"] * \
-                                     np.max(q_next.numpy(), axis=(1)) + \
-                                     self.__hash["gamma1"] * scores.numpy()
+logger = logging.getLogger('py2048')
 
 
-    history = m1.fit(tf2.constant(states),
-                     q_target,
-                     epochs=self.__hash["training_epochs"])
+class Agent:
+    def __init__(
+            self,
+            batch_size=10000,
+            mem_size=50000,
+            input_dims=[16],
+            lr=0.001,
+            gamma=0.99,
+            epsilon=1,
+            epsilon_dec=1e-3,
+            epsilon_min=0.01,
+            model_load_file=None,
+            model_save_file='model.h5',
+            model_auto_save=True,
+            log_dir="/tmp/logs",
+            tf_proc_debug=False,
+            training_epochs=1,
+            **kwargs
+        ):
+        self.__hash = {}
+        self.batch_size = batch_size
+        self.mem_size = mem_size
+        self.input_dims = input_dims[::]
+        self.lr = lr
+        self.gamma = gamma
+        self.n_actions = 4
+        self.epsilon = epsilon
+        self.epsilon_dec = epsilon_dec
+        self.epsilon_min = epsilon_min
+        self.model_load_file = model_load_file or model_save_file
+        self.model_save_file = model_save_file
+        self.model_auto_save = model_auto_save
+        self.log_dir = log_dir
+        self.tf_proc_debug = tf_proc_debug
+        self.training_epochs = training_epochs
 
-    # TB log data
-    file_writer = tf2.summary.create_file_writer(self.__hash['log_dir'])
-    file_writer.set_as_default()
-    tf2.summary.scalar('Game score', data=self.__hash['last_game_score'], step=run)
-    tf2.summary.scalar('Game: moves to completion', data=self.__hash['last_game_num_moves'], step=run)
+        for k, v in kwargs.items():
+            self.__hash[k] = kwargs[k]
 
-    for name in history.history.keys():
-      tf2.summary.scalar(name, data=history.history[name][-1], step=run)
+        """
+        Special parameters.
+        """
+        if "model" not in self.__hash.keys():
+            self.__create_default_model()
+        else:
+            self.model = self.__hash["model"]
 
-    # Close the writer
-    file_writer.close()
+        if "episode_db" not in self.__hash.keys():
+            self.episode_db = episodes.EdpisodeDB(
+                self.mem_size,
+                self.input_dims,
+                tf_proc_debug=self.tf_proc_debug
+            )
+        else:
+            self.episode_db = self.__hash["episode_db"]
 
-    # Adjust the epsilon
-    self.__hash["epsilon"] = self.__hash["epsilon"]  - self.__hash["epsilon_dec"] \
-      if self.__hash["epsilon"] >self.__hash["epsilon_min"] else self.__hash["epsilon_min"]
+        self.last_game_score = 0
 
-  def learn_on_repeat(self, n_games=1):
-    log = self.__hash["logger"]
-    min_score = 0
-    max_score = 0
-    avg_score = 0.0
-    sum_scores = 0
-    run_num = 0
+        self.tensorboard_callback = tf.keras.callbacks.TensorBoard(
+            log_dir=self.log_dir,
+            histogram_freq=1,
+            profile_batch = '500,520'
+        )
 
-    for i in range(n_games):
-      self.learn(i)
-      self.play_game(self.action_greedy_epsilon)
-      if self.__hash["model_auto_save"]: self.save_model()
+        self.training_histories = []
 
-      min_score = self.__hash["last_game_score"] if i == 0 \
-        else min(min_score, self.__hash["last_game_score"] )
+    def __create_default_model(self):
+        logger.debug("Creating default model")
 
-      max_score = max(max_score, self.__hash["last_game_score"] )
-      sum_scores += self.__hash["last_game_score"]
-      run_num = i+1
-      avg_score = sum_scores / run_num
+        model: tf.keras.models.Sequential = tf.keras.models.Sequential([
+            tf.keras.layers.Dense(16, activation=None),
+            tf.keras.layers.Dense(256, activation='relu'),
+            tf.keras.layers.Dense(256, activation='relu'),
+            tf.keras.layers.Dense(4, activation=None)
+        ])
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+            loss=tf.keras.losses.mse
+        )
 
-      log.generic_output(field_names = ["Learning run",
-                                        "Min score",
-                                        "Average score",
-                                        "Last score",
-                                        "Max score"], \
-                            field_content = [run_num.__str__(),
-                                             min_score.__str__(),
-                                             avg_score.__str__(),
-                                             self.__hash["last_game_score"].__str__(),
-                                             max_score.__str__()])
+        self.model = model
 
+    def learn(self):
+        self.accumulate_episode_data()
+        ep_db = self.episode_db
+        m1 = self.model
 
-  def accumulate_episode_data(self):
-    ep_db = self.__hash["episode_db"]
-    # Bail if there's nothing to do.
-    if ep_db.mem_cntr >= self.__hash["batch_size"]: return
+        states, states_, actions, rewards, dones = \
+            ep_db.get_random_data_batch(self.batch_size)
 
-    log = self.__hash["logger"]
-    log.freeform_output("Initial data accumulation. Collection size = " +
-                        self.__hash["mem_size"].__str__() +
-                         " episodes.")
+        q_eval = tf.Variable(tf.constant(m1.predict(states.numpy())))
+        q_next = tf.Variable(tf.constant(m1.predict(states_.numpy())))
+        q_target = q_eval.numpy()
+        batch_index = np.arange(self.batch_size)
+        q_target[batch_index, actions] = rewards + self.gamma * \
+             np.max(q_next.numpy(), axis=(1))
 
-    while ep_db.mem_cntr < self.__hash["batch_size"]:
-      self.play_game(self.action_random)
+        logger.debug("learn: states: %s", states.shape)
+        logger.debug("learn: q_target: %s", q_target.shape)
 
-    log.freeform_output("Initial data accumulation completed.")
+        history = m1.fit(
+            states.numpy(),
+            q_target,
+            callbacks=[self.tensorboard_callback],
+            epochs=self.training_epochs
+        )
 
-  def play_game(self, action_choice):
-    game1 = game.Game()
-    state = None
-    action = None
-    state = None
-    state_ = None
-    reward = None
-    score = None
-    e_db = self.__hash["episode_db"]
-    num_moves = 0
+        self.training_histories.append(history)
 
-    while not game1.game_over():
-      action = action_choice(game1)
-      state = np.matrix.flatten( game1.state() )
-      reward = game1.do_action(action)
-      state_ = np.matrix.flatten( game1.state() )
-      if max(abs(state - state_)) > 0 : num_moves += 1
+        # Adjust the epsilon
+        if self.epsilon > self.epsilon_min:
+            self.epsilon = self.epsilon - self.epsilon_dec
 
-      e_db.store_episode(
-        episodes.Episode(state=state,
-                         next_state=state_,
-                         action=action,
-                         reward=reward,
-                         score=game1.score(),
-                         done=game1.game_over()))
+    def learn_on_repeat(self, n_games=1):
+        min_score = 0
+        max_score = 0
+        avg_score = 0.0
+        sum_scores = 0
+        run_num = 0
 
-    self.__hash["last_game_score"] = game1.score()
-    self.__hash['last_game_num_moves'] = num_moves
+        for i in range(n_games):
+            self.learn()
+            self.play_game(self.action_greedy_epsilon)
 
-  def action_random(self, curr_game):
-    return np.random.choice(curr_game.available_actions())
+            if self.model_auto_save:
+                self.save_model()
 
-  def action_greedy_epsilon(self, curr_game):
-    if np.random.random() < self.__hash["epsilon"]:
-      return self.action_random(curr_game)
+            if i == 0:
+                min_score = self.last_game_score
+            else:
+                min_score = min(min_score, self.last_game_score)
+            max_score = max(max_score, self.last_game_score)
+            sum_scores += self.last_game_score
+            run_num += 1
+            avg_score = sum_scores / run_num
 
-    state = curr_game.state()
-    state = np.matrix.reshape(state, (1, 16))
-    m1 = self.__hash["model"]
-    actions = m1.predict(state)
-    actions = actions[0][curr_game.available_actions()]
-    return np.argmax(actions)
+            logger.info('Step %d: Run=%s min=%s avg=%s last=%s max=%s',
+                        i, run_num, max_score, avg_score, self.last_game_score, max_score)
 
-  def save_model(self):
-    m1 = self.__hash["model"]
-    m1.save(self.__hash["model_save_file"])
+    def accumulate_episode_data(self):
+        ep_db = self.episode_db
+        # Bail if there's nothing to do.
+        if ep_db.mem_cntr >= self.batch_size:
+            return
 
-  def load_model(self):
-    self.__hash["model"] = tf2.keras.models.load_model(self.__hash["model_load_file"])
+        logger.debug("Initial data accumulation. Collection size = %s episodes.",
+                     self.mem_size)
+        while ep_db.mem_cntr < self.batch_size:
+            self.play_game(self.action_random)
+        logger.debug("Initial data accumulation completed.")
+
+    def play_game(self, action_choice):
+        game1 = game.Game()
+        state = None
+        action = None
+        state = None
+        state_ = None
+        reward = None
+        score = None
+        e_db = self.episode_db
+
+        while not game1.game_over():
+            action = action_choice(game1)
+            state = np.matrix.flatten(game1.state())
+            reward = game1.do_action(action)
+            state_ = np.matrix.flatten(game1.state())
+            episode = episodes.Episode(
+                state=state,
+                next_state=state_,
+                action=action,
+                reward=reward,
+                score=game1.score(),
+                done=game1.game_over()
+            )
+            e_db.store_episode(episode)
+
+        self.last_game_score = game1.score()
+
+    def action_random(self, curr_game):
+        return np.random.choice(curr_game.available_actions())
+
+    def action_greedy_epsilon(self, curr_game):
+        if np.random.random() < self.epsilon:
+            return self.action_random(curr_game)
+
+        state = curr_game.state()
+        state = np.matrix.reshape(state, (1, 16))
+        m1 = self.model
+        actions = m1.predict(state)
+        actions = actions[0][curr_game.available_actions()]
+        return np.argmax(actions)
+
+    def save_model(self):
+        m1 = self.model
+        m1.save(self.model_save_file)
+
+    def load_model(self):
+        self.model = tf.keras.models.load_model(self.model_load_file)
--- py_2048_rl/flex_rl_model/flex_rl.py
+++ py_2048_rl/flex_rl_model/flex_rl.py
@@ -1,14 +1,22 @@
+import os
 import argparse
-import nn_model
-import agent
+import logging
+
+import tensorflow as tf
+
+from py_2048_rl.flex_rl_model.agent import Agent
+
 
 # Making sure we are running the main routine.
 if __name__ != "__main__":
   exit()
 
+logger = logging.getLogger('py2048')
+tf_logger = logging.getLogger('tensorflow')
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--learn_runs', type=int, default=100, help='Number of model learn runs')
-parser.add_argument('--batch_size', type=int, default=10000,
+parser.add_argument('--learn-runs', type=int, default=100, help='Number of model learn runs')
+parser.add_argument('--batch-size', type=int, default=10000,
                     help='Training batch selection size (in number of episodes')
 parser.add_argument('--mem_size', type=int, default=50000,
                     help='Learning episode DB size (in number of episodes')
@@ -18,38 +26,69 @@ parser.add_argument('--gamma', type=float, default=0.99,
                     help='Gamma')
 parser.add_argument('--epsilon', type=float, default=1.0,
                     help='Epsilon')
-parser.add_argument('--epsilon_min', type=float, default=0.01,
+parser.add_argument('--epsilon-min', type=float, default=0.01,
                     help='Epsilon - start value')
-parser.add_argument('--epsilon_dec', type=float, default=0.001,
+parser.add_argument('--epsilon-dec', type=float, default=0.001,
                     help='Epsilon - step value')
-parser.add_argument('--log_dir', default="/app/logs",
-                    help='Tensorboard log directory')
-parser.add_argument('--model_load_file', default=None,
+
+parser.add_argument('--model-load-file', default=None,
                     help='Model load file path (h5)')
-parser.add_argument('--model_save_file', default='model.h5',
+parser.add_argument('--model-save-file', default='model.h5',
                     help='Model save file path (h5)')
-parser.add_argument('--training_epochs', type=int, default=1,
+parser.add_argument('--training-epochs', type=int, default=1,
                     help='Number of epoch rns for every model training run')
-parser.add_argument('--tf_proc_debug', default=False, type=bool,
+parser.add_argument('--model-disable-autosave', default=True, action="store_false",
+                    dest="model_auto_save")
+
+parser.add_argument('--log-dir', default="/tmp/logs",
+                    help='Tensorboard log directory')
+parser.add_argument('--tf-log-device', default=False, action="store_true",
                     help='Determines whether TF compute device info is displayed. Default = False')
+parser.add_argument('--tf-dump-debug-info', default=False, action="store_true")
+parser.add_argument('--tf-profiler-port', default=0, type=int)
+parser.add_argument('--verbose', '-v', default=3, type=int)
+parser.add_argument('--tf-verbose', '-tfv', default=2, type=int)
 
 args = parser.parse_args()
 
-nn_mod = nn_model.NN_Model(batch_size = args.batch_size,
-                           lr=args.lr,
-                           gamma=args.gamma,
-                           epsilon=args.epsilon,
-                           epsilon_min=args.epsilon_min,
-                           epsilon_dec=args.epsilon_dec,
-                           taining_epochs=args.training_epochs,
-                           model_load_file=args.model_load_file,
-                           model_save_file=args.model_save_file,
-                           log_dir=args.log_dir,
-                           tf_proc_debug=args.tf_proc_debug)
+log_verbose = 60 - (args.verbose*10)
+log_handler = logging.StreamHandler()
+log_handler.setLevel(log_verbose)
+logger.addHandler(log_handler)
+logger.setLevel(log_verbose)
 
-agent = nn_mod.get_param("agent")
+tf_log_verbose = 60 - (args.tf_verbose*10)
+tf_logger.setLevel(tf_log_verbose)
+os.environ.setdefault('TF_CPP_MIN_LOG_LEVEL', str(args.tf_verbose))
+
+logger.debug('Config: %s', vars(args))
+
+tf.debugging.set_log_device_placement(args.tf_log_device)
+if args.log_dir and args.tf_dump_debug_info:
+    tf.debugging.experimental.enable_dump_debug_info(
+        args.log_dir,
+        tensor_debug_mode="FULL_HEALTH",
+        circular_buffer_size=-1
+    )
+if args.tf_profiler_port:
+    tf.profiler.experimental.server.start(args.tf_profiler_port)
+
+agent = Agent(
+    batch_size=args.batch_size,
+    mem_size=args.mem_size,
+    # input_dims=args.input_dims,
+    input_dims=[16],
+    lr=args.lr,
+    gamma=args.gamma,
+    epsilon=args.epsilon,
+    epsilon_dec=args.epsilon_dec,
+    epsilon_min=args.epsilon_min,
+    model_load_file=args.model_load_file,
+    model_save_file=args.model_save_file,
+    model_auto_save=args.model_auto_save,
+    log_dir=args.log_dir,
+    tf_proc_debug=args.tf_log_device,
+    training_epochs=args.training_epochs,
+)
 
 agent.learn_on_repeat(args.learn_runs)
-
-
-
